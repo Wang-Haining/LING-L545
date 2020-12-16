a7defd0a3dd7cd44405ba04414ac05c343bbe5a2
chipmunk-neighbor <whn395436129@gmail.com>
2020-12-15 23:53:27 -0500

    done the segmenter.py

---
+++ 01_Tokenisation/segmenter.py
@@ -0,0 +1,39 @@
+"""
+This module is used to build a naive segmenter.
+
+Questions
+1. How should you segment sentences with semi-colon? As a single sentence or as two sentences? Should it depend on
+context?
+Answer: Yes, context matters. It depends on whether the initial after the semi-colon is capitalized or not and whether
+the following clause is grammarly complete.
+
+2. Should sentences with ellipsis... be treated as a single sentence or as several sentences?
+Answer: I always treat ellipsis as the ending of a single sentence.
+
+3. If there is an exclamation after the first word in the sentence should it be a separate sentence? How about if there
+is a comma?
+Answer: If there is an exclamation after the first word, then there are two sentences. But if there is a comma instead,
+it should be one sentence. The first situation is common when the first word is a vocative ("John! watch out!"). The
+second situation is very common with a adjective ("Frankly, there is no evidence.").
+
+4.Can you think of some hard tasks for the segmenter?
+Answer: If there many abbreviations in the middle of a sentence, it would cause confusion to a naive segmenter.
+"""
+import sys
+
+
+def segmenter(raw):
+    """
+    Replaces every full stop '.' followed by a space ' ' with a full stop and a newline character '\n'.
+
+    Args:
+        raw: a string
+    Return:
+        a segmented string
+    """
+    print(raw.replace(". ", ".\n"))
+
+
+if __name__ == "__main__":
+    raw = sys.argv[1]
+    segmenter(raw)
--- 01_Tokenisation/wer.py
+++ 01_Tokenisation/wer.py
@@ -1,59 +1,63 @@
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 #!/usr/bin/env python
 
 import sys
 import numpy
 
+
 def editDistance(r, h):
-    '''
+    """
     This function is to calculate the edit distance of reference sentence and the hypothesis sentence.
 
     Main algorithm used is dynamic programming.
 
-    Attributes: 
+    Attributes:
         r -> the list of words produced by splitting reference sentence.
         h -> the list of words produced by splitting hypothesis sentence.
-    '''
-    d = numpy.zeros((len(r)+1)*(len(h)+1), dtype=numpy.uint8).reshape((len(r)+1, len(h)+1))
-    for i in range(len(r)+1):
+    """
+    d = numpy.zeros((len(r) + 1) * (len(h) + 1), dtype=numpy.uint8).reshape(
+        (len(r) + 1, len(h) + 1)
+    )
+    for i in range(len(r) + 1):
         d[i][0] = i
-    for j in range(len(h)+1):
+    for j in range(len(h) + 1):
         d[0][j] = j
-    for i in range(1, len(r)+1):
-        for j in range(1, len(h)+1):
-            if r[i-1] == h[j-1]:
-                d[i][j] = d[i-1][j-1]
+    for i in range(1, len(r) + 1):
+        for j in range(1, len(h) + 1):
+            if r[i - 1] == h[j - 1]:
+                d[i][j] = d[i - 1][j - 1]
             else:
-                substitute = d[i-1][j-1] + 1
-                insert = d[i][j-1] + 1
-                delete = d[i-1][j] + 1
+                substitute = d[i - 1][j - 1] + 1
+                insert = d[i][j - 1] + 1
+                delete = d[i - 1][j] + 1
                 d[i][j] = min(substitute, insert, delete)
     return d
 
+
 def getStepList(r, h, d):
-    '''
+    """
     This function is to get the list of steps in the process of dynamic programming.
 
-    Attributes: 
+    Attributes:
         r -> the list of words produced by splitting reference sentence.
         h -> the list of words produced by splitting hypothesis sentence.
         d -> the matrix built when calulating the editting distance of h and r.
-    '''
+    """
     x = len(r)
     y = len(h)
     list = []
     while True:
-        if x == 0 and y == 0: 
+        if x == 0 and y == 0:
             break
-        elif x >= 1 and y >= 1 and d[x][y] == d[x-1][y-1] and r[x-1] == h[y-1]: 
+        elif x >= 1 and y >= 1 and d[x][y] == d[x - 1][y - 1] and r[x - 1] == h[y - 1]:
             list.append("e")
             x = x - 1
             y = y - 1
-        elif y >= 1 and d[x][y] == d[x][y-1]+1:
+        elif y >= 1 and d[x][y] == d[x][y - 1] + 1:
             list.append("i")
             x = x
             y = y - 1
-        elif x >= 1 and y >= 1 and d[x][y] == d[x-1][y-1]+1:
+        elif x >= 1 and y >= 1 and d[x][y] == d[x - 1][y - 1] + 1:
             list.append("s")
             x = x - 1
             y = y - 1
@@ -63,16 +67,17 @@ def getStepList(r, h, d):
             y = y
     return list[::-1]
 
+
 def alignedPrint(list, r, h, result):
-    '''
+    """
     This funcition is to print the result of comparing reference and hypothesis sentences in an aligned way.
-    
+
     Attributes:
         list   -> the list of steps.
         r      -> the list of words produced by splitting reference sentence.
         h      -> the list of words produced by splitting hypothesis sentence.
         result -> the rate calculated based on edit distance.
-    '''
+    """
     print("REF:", end=" ")
     for i in range(len(list)):
         if list[i] == "i":
@@ -81,7 +86,7 @@ def alignedPrint(list, r, h, result):
                 if list[j] == "d":
                     count += 1
             index = i - count
-            print(" "*(len(h[index])), end=" ")
+            print(" " * (len(h[index])), end=" ")
         elif list[i] == "s":
             count1 = 0
             for j in range(i):
@@ -94,7 +99,7 @@ def alignedPrint(list, r, h, result):
                     count2 += 1
             index2 = i - count2
             if len(r[index1]) < len(h[index2]):
-                print(r[index1] + " " * (len(h[index2])-len(r[index1])), end=" ")
+                print(r[index1] + " " * (len(h[index2]) - len(r[index1])), end=" ")
             else:
                 print(r[index1], end=" "),
         else:
@@ -125,7 +130,7 @@ def alignedPrint(list, r, h, result):
                     count2 += 1
             index2 = i - count2
             if len(r[index1]) > len(h[index2]):
-                print(h[index2] + " " * (len(r[index1])-len(h[index2])), end=" ")
+                print(h[index2] + " " * (len(r[index1]) - len(h[index2])), end=" ")
             else:
                 print(h[index2], end=" ")
         else:
@@ -143,14 +148,14 @@ def alignedPrint(list, r, h, result):
                 if list[j] == "i":
                     count += 1
             index = i - count
-            print("D" + " " * (len(r[index])-1), end=" ")
+            print("D" + " " * (len(r[index]) - 1), end=" ")
         elif list[i] == "i":
             count = 0
             for j in range(i):
                 if list[j] == "d":
                     count += 1
             index = i - count
-            print("I" + " " * (len(h[index])-1), end=" ")
+            print("I" + " " * (len(h[index]) - 1), end=" ")
         elif list[i] == "s":
             count1 = 0
             for j in range(i):
@@ -163,9 +168,9 @@ def alignedPrint(list, r, h, result):
                     count2 += 1
             index2 = i - count2
             if len(r[index1]) > len(h[index2]):
-                print("S" + " " * (len(r[index1])-1), end=" ")
+                print("S" + " " * (len(r[index1]) - 1), end=" ")
             else:
-                print("S" + " " * (len(h[index2])-1), end=" ")
+                print("S" + " " * (len(h[index2]) - 1), end=" ")
         else:
             count = 0
             for j in range(i):
@@ -175,10 +180,11 @@ def alignedPrint(list, r, h, result):
             print(" " * (len(r[index])), end=" ")
     print("\nWER: " + result)
 
+
 def wer(r, h):
     """
     This is a function that calculate the word error rate in ASR.
-    You can use it like this: wer("what is it".split(), "what is".split()) 
+    You can use it like this: wer("what is it".split(), "what is".split())
     """
     # build the matrix
     d = editDistance(r, h)
@@ -205,14 +211,15 @@ def wer_modified(r, h):
 
     # print the result in aligned way
     result = float(d[len(r)][len(h)]) / len(r) * 100
-    result = float(str("%.2f" % result) )
+    result = float(str("%.2f" % result))
     return result
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     filename1 = sys.argv[1]
     filename2 = sys.argv[2]
-    with open(filename1, 'r', encoding="utf8") as ref:
+    with open(filename1, "r", encoding="utf8") as ref:
         r = ref.read().split()
-    with open(filename2, 'r', encoding="utf8") as hyp:
+    with open(filename2, "r", encoding="utf8") as hyp:
         h = hyp.read().split()
-    wer(r, h)   
+    wer(r, h)
